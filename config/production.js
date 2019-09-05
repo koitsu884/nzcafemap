ac3a06e101b3c75d24084211ff39c2a9cf062ebb
koitsu884 <koitsu884@hotmail.com>
2019-09-05 15:40:57 +1200

    Updates

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     "jwtPrivateKey": process.env.JWT_KEY,
-    "db": process.env.DB_HOST,
+    "db": process.env.MONGODB_URI,
     "cloudinaryName": process.env.CLOUDINARY_NAME,
     "cloudinaryAPIKey": process.env.CLOUDINARY_KEY,
     "cloudinarySecret": process.env.CLOUDINARY_SECRET
