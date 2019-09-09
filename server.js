e9d63a207f3d8e5f86bdcba5b828e2432eaafdf1
koitsu884 <koitsu884@hotmail.com>
2019-09-09 15:17:12 +1200

    Updates

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 require('./startup/routes')(app);
 //Front end
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
